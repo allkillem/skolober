35847cad778fb8b6917f3b9b51527d70c169a37e
allkillem <poubelle@keaweb.fr>
2018-12-19 09:56:27 +0100

    Bootstrap
    ajout de bootstrap

--- _dev/js/theme.js
+++ _dev/js/theme.js
@@ -1,3 +1,12 @@
 /**
  * This is the entry point for specific javascript of theme
  */
+
+/** ajout bootstrap **/
+
+require('bootstrap');
+require('../css/theme.scss');
+
+$(function () {
+    // DOM READY
+});
